cab023f1315187016cebaad5554a6e5d1da8f0cb
kennygunderman <kennygunderman@gmail.com>
2019-09-13 20:17:56 -0400

    Add Login as root

--- Swaray/Util/Consts.swift
+++ Swaray/Util/Consts.swift
@@ -5,5 +5,12 @@
 //  Created by Kenny Gunderman on 9/13/19.
 //  Copyright © 2019 Kenny Gunderman. All rights reserved.
 //
+// This File is responsible for all constant values
+// used throught the application.
+
 
 import Foundation
+
+struct Constants {
+    static let loginTitle: String = "Login"
+}
