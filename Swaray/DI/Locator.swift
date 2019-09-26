57606e42f4def20e1ccd51503f64748717d23dc2
kennygunderman <kennygunderman@gmail.com>
2019-09-25 21:53:05 -0400

    Remove method to add Mocks in Locator

--- Swaray/DI/Locator.swift
+++ Swaray/DI/Locator.swift
@@ -14,7 +14,6 @@ import Foundation
 fileprivate typealias TypeInstantator = Any
 enum Locator {
     private static var instantiators: [String: TypeInstantator] = [:]
-    private static var mockInstantiators: [String: TypeInstantator?] = [:]
     static var isTestEnvironment = false
     
     static func bind<T>(
@@ -27,27 +26,7 @@ enum Locator {
     
     private static func instance<T>() -> T {
         let key = String(describing: T.self)
-        if self.isTestEnvironment {
-            guard let instantiator = mockInstantiators[key] as? () -> T else {
-                fatalError("Type \(key) unmocked in test!")
-            }
-            return instantiator()
-        }
         let instantiator = instantiators[key] as! () -> T
         return instantiator()
-    }
-    
-    //todo move to a `Mock impl` (Locator_M.swift)
-    static func mock<T>(_ type: T.Type, instantiator: @escaping () -> T) {
-        mockInstantiators[String(describing: type)] = instantiator
-    }
-    
-    //todo move to a `Mock impl` (Locator_M.swift)
-    static func removeMocks() {
-        for (key, _) in mockInstantiators {
-            mockInstantiators[key] = nil
-        }
-        
-        mockInstantiators = [:]
     }
 }
