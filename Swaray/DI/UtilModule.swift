50ebebfae1b805b7fb598110b297c18fbbd27350
kennygunderman <kennygunderman@gmail.com>
2019-11-10 20:28:50 -0500

    Add DateFormatter dependency

---
+++ Swaray/DI/UtilModule.swift
@@ -0,0 +1,17 @@
+//
+//  UtilModule.swift
+//  Swaray
+//
+//  Created by Kenny Gunderman on 11/10/19.
+//  Copyright © 2019 Kenny Gunderman. All rights reserved.
+//
+
+import Foundation
+
+fileprivate let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "M/d/yyyy"
+    return formatter
+}()
+
+let getDateFormatter = Locator.bind(DateFormatter.self) { dateFormatter }
